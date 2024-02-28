a0ef383fa2eb270b1028c7f2a354d5d770424585
pramod <gopinath.pramod@gmail.com>
2024-02-28 23:45:45 +0530

    First Commit

---
+++ src/Components/Banner.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import './Banner.css'
+import foot from './DigitalFootprint.png'
+
+/**
+* @author
+* @function Legimage
+**/
+
+export const Legimage = (props) => {
+  return(
+    <div className='app'>
+      <h1>Foot Presure Meter</h1>
+        
+        <div className='main'>
+            <div className='back'>
+                <img src={foot}/>
+            </div>
+            <div className='Hover'>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items '></div>
+                <div className='items '></div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items  colorchangeingclass '>11</div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items '></div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items colorchangeingclass'>17</div>
+                <div className='items'></div>
+                <div className='items  colorchangeingclass'>19</div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items  colorchangeingclass'>28</div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items colorchangeingclass'>28</div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items '></div>
+                <div className='items '></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+                <div className='items'></div>
+
+
+            </div>
+        </div>
+    </div>
+   )
+
+ }
